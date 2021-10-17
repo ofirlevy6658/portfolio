153191ec8137ea6709b38acde5f69355184c027d
ofirlevy6658 <ofirlevy6658@gmail.com>
2021-10-17 12:15:58 +0300

    tool tip for icons

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import ReactTooltip from "react-tooltip";
 import "./home.scss";
 
 export const Home = () => {
@@ -14,30 +14,59 @@ export const Home = () => {
 					<p>
 						& <span>BSc in Computer Science </span>
 					</p>
-					{/* <button className="btn">About me</button> */}
 				</div>
 				<div className="icons">
 					<a
 						href="https://drive.google.com/file/d/1f7cFpL__WiSbsTeAJeg1qYkTTNjwWFul/view?usp=sharing"
 						target="_blank"
 						rel="noreferrer"
+						data-tip
+						data-for="c.v"
 					>
 						<i className="fas fa-file-alt fa-3x"></i>
 					</a>
+					<ReactTooltip
+						id="c.v"
+						type="dark"
+						effect="solid"
+						offset={{ top: 20 }}
+					>
+						<span>C.V</span>
+					</ReactTooltip>
 					<a
 						href="https://github.com/ofirlevy6658"
 						target="_blank"
 						rel="noreferrer"
+						data-tip
+						data-for="github-profile"
 					>
 						<i className="fab fa-github fa-3x"></i>
 					</a>
+					<ReactTooltip
+						id="github-profile"
+						type="dark"
+						effect="solid"
+						offset={{ top: 20 }}
+					>
+						<span>Github Profile</span>
+					</ReactTooltip>
 					<a
 						href="https://www.linkedin.com/in/ofirlevy6658/"
 						target="_blank"
 						rel="noreferrer"
+						data-tip
+						data-for="linkedin-profile"
 					>
 						<i className="fab fa-linkedin-in fa-3x"></i>
 					</a>
+					<ReactTooltip
+						id="linkedin-profile"
+						type="dark"
+						effect="solid"
+						offset={{ top: 20 }}
+					>
+						<span>Linkedin Profile</span>
+					</ReactTooltip>
 				</div>
 			</div>
 		</div>
